9335160b0bfd942303519e80b02064cd73400b02
Nilanshu Garhewal <nilanshugarhewal1@gmail.com>
2025-09-29 13:13:19 +0530

    PROJECT PAGE AND SUB PROJECT PAGE IS NOW RESPONSIVE FOR MOBILE AND TABLET DEVICES.

--- src/app/projects/ProjectHeroSection.tsx
+++ src/app/projects/ProjectHeroSection.tsx
@@ -4,10 +4,10 @@ const ProjectHeroSection = () => {
   return (
     <div className="project-hero-section">
       <div className="project-hero-heading-wrapper">
-        <span className="project-hero-heading">My Projects</span>
-        <span className="project-hero-des">
+        <div className="project-hero-heading">My Projects</div>
+        <div className="project-hero-des">
           Everyday tools that help people live a little easier.
-        </span>
+        </div>
       </div>
     </div>
   );
